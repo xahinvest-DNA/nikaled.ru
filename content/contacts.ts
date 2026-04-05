1a5725c18fcac5a1e365558f7952692150798229
xahinvest-DNA <xahinvest-dna@users.noreply.github.com>
2026-04-05 14:19:50 +0300

    Обновлен номер телефона на сайте

--- content/contacts.ts
+++ content/contacts.ts
@@ -1,10 +1,10 @@
 export const contacts = {
   companyName: "Наружная реклама Воронеж",
   city: "Воронеж",
-  phoneRaw: "+79601202626",
-  phoneDisplay: "+7 (960) 120-26-26",
+  phoneRaw: "+79042122406",
+  phoneDisplay: "+7 (904) 212-24-06",
   telegramUrl: "https://t.me/nikaled_manager",
-  whatsappUrl: "https://wa.me/79601202626",
+  whatsappUrl: "https://wa.me/79042122406",
   address: "г. Воронеж, ул. Производственная, 12",
   workHours: "Пн-Сб: 09:00-19:00"
 };
